80227d9d1f5e7078c534b2904bcec1beed3e4631
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-06-12 00:04:43 -0700

    User inputs with console log printing working

---
+++ README.md
@@ -0,0 +1,3 @@
+# Link to deployed webpage: 
+https://jessicablank.github.io/password-generator/
+Criteria: When the user clicks the "generate code" button, the app asks user about criteria for a randomly generated password based on that criteria. 
--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,34 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Ask user to input criteria of character length, lowercase, UPPERCASE,numeric, and/or special characters.
+var charLength = prompt("Enter the number of characters required between 8 and 128");
+if (charLength < 8 || charLength > 128) {
+  alert("Please enter a number between 8 and 128");
+}
+
+var charLowercase = confirm("Do you want the password to contain lowercase letters?");
+
+var charUppercase = confirm("Do you want the password to contain UPPERCASE letters?");
+
+var charNumeric = confirm("Do you want the password to contain numbers?");
+
+var charSpecial = confirm("Do you want the password to contain special characters?");
+
+
+
+//Making sure variables are logged
+console.log(charLength);
+console.log(charLowercase);
+console.log(charUppercase);
+console.log(charNumeric);
+console.log(charSpecial);
+
+//Password generation conditions
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
