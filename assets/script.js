010cbd7fc637c02e89a69972582ab2bb06780528
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-06-12 02:39:28 -0700

    password generator is working as expected

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//Ask user to input criteria of character length, lowercase, UPPERCASE,numeric, and/or special characters.
-var charLength = prompt("Enter the number of characters required between 8 and 128");
-if (charLength < 8 || charLength > 128) {
-  alert("Please enter a number between 8 and 128");
-}
-
-var charLowercase = confirm("Do you want the password to contain lowercase letters?");
-
-var charUppercase = confirm("Do you want the password to contain UPPERCASE letters?");
-
-var charNumeric = confirm("Do you want the password to contain numbers?");
-
-var charSpecial = confirm("Do you want the password to contain special characters?");
-
-
-
-//Making sure variables are logged
-console.log(charLength);
-console.log(charLowercase);
-console.log(charUppercase);
-console.log(charNumeric);
-console.log(charSpecial);
-
-//Password generation conditions
-
-
-
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -40,3 +12,73 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+//declare variables 
+
+var confirmLength = "";
+var confirmLower;
+var confirmUpper;
+var confirmNumeric;
+var confirmSpecial;
+
+
+  
+  
+
+
+//set arrays for other pw criteria
+var charNumeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var charSpecial = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var charLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var charUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+//function begins
+function generatePassword() {
+var confirmLenth = prompt("Enter the number of characters required between 8 and 128");
+
+
+
+var confirmLower = confirm ("Do you want the password to contain lowercase letters?");
+var confirmUpper = confirm("Do you want the password to contain UPPERCASE letters?");
+var confirmNumeric = confirm("Do you want the password to contain numbers?");
+var confirmSpecial = confirm("Do you want the password to contain special characters?");
+
+
+
+
+var passwordCharacters = []
+
+if (confirmLower) {
+  passwordCharacters = passwordCharacters.concat(charLower)
+}
+      
+if (confirmUpper) {
+  passwordCharacters = passwordCharacters.concat(charUpper)
+}
+
+if (confirmSpecial) {
+  passwordCharacters = passwordCharacters.concat(charSpecial)
+}
+
+if (confirmNumeric) {
+  passwordCharacters = passwordCharacters.concat(charNumeric)
+}
+
+console.log(passwordCharacters)
+
+//Password generation conditions
+      
+      var randomPassword = ""
+      
+      for (var i = 0; i < confirmLenth; i++) {
+        randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+        console.log(randomPassword)
+      }
+     return randomPassword;
+
+   }
+
+    
