f8fae96222346dfb173b6fd9524c0d954a2a3167
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-10-13 17:53:44 -0700

    working as expected

--- assets/pwGen.js
+++ assets/pwGen.js
@@ -1,5 +1,6 @@
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
+let pwLength;
 
 // Write password to the #password input
 writePassword = () => {
@@ -8,9 +9,58 @@ writePassword = () => {
   passwordText.value = password;
 }
 
+//Slider
+const slider = document.getElementById("passwordLength");
+const output = document.getElementById("passwordLengthView");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+  pwLength = this.value;
+}
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//Declare variables for user input
+//Set arrays for characters
+const numbers = "0123456789";
+const specialChars = "!@#$%^&*()_+~`|}{[]:;?/-=";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+//Receive User Inputs
+const lowercaseInput = document.getElementById("lowerCase");
+const uppercaseInput = document.getElementById("upperCase");
+const numbersInput = document.getElementById("numbers");
+const specialCharsInput = document.getElementById("specChars");
 
 
+//Generate a Random Password!
+function generatePassword(){
+  randomPassword = "";
+  passwordCharSet="";
+  if(lowercaseInput.checked){
+    passwordCharSet += lowerCase;
+  }
+  if (uppercaseInput.checked){
+    passwordCharSet += upperCase;
+  } 
+  if (numbersInput.checked){
+    passwordCharSet += numbers;
+  }
+  if (specialCharsInput.checked){
+    passwordCharSet += specialChars;
+  }
+
+  for (let i=0; i < pwLength; i++){
+    randomPassword += passwordCharSet.charAt(
+      Math.floor(Math.random() * passwordCharSet.length)
+    );
+  }
+
+  return randomPassword;
+
+}
