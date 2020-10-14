803f58c8eb0d6d160b73fa70a027eb286f6b6f8f
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-10-14 15:57:55 -0700

    removed old script.js file and unneeded tool tips

--- assets/pwGen.js
+++ assets/pwGen.js
@@ -1,5 +1,7 @@
-// This is the refactored Javascript Code.
+
 const generateBtn = document.querySelector("#generate");
+
+//Default slider length
 let pwLength = 94;
 
 // Write password to the #password input
