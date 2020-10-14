782a76b3a28cda4db629f41ffa02476a0a05c753
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-10-13 18:06:38 -0700

    eeep! added Octocat arm

--- assets/copyBtn.js
+++ assets/copyBtn.js
@@ -1,13 +1,13 @@
 const input = document.getElementById('password');
 const alert = document.getElementsByClassName('alert');
 
-
 function copyPassword() {
   //check for a new password, otherwise alert an error
   if (input.value) {
   input.select();
   input.setSelectionRange(0, 99999)
   document.execCommand("copy");
+
   } else {
     $('.alert').removeClass('d-none').addClass('show');
   }
