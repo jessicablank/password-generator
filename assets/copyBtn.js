17685d380724b0198b55979459bc525a1bc06717
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-10-15 10:55:52 -0700

    Added success message for copying password

--- assets/copyBtn.js
+++ assets/copyBtn.js
@@ -10,10 +10,19 @@ const alert = document.getElementsByClassName('alert');
 function copyPassword() {
   //check for a new password, otherwise alert an error
   if (input.value) {
+    //Select the Password in the Text Field
   input.select();
   input.setSelectionRange(0, 99999)
+  //Copy to ClipBoard
   document.execCommand("copy");
+  // Get the snackbar DIV
+  let success = document.getElementById("snackbar");
 
+  // Add the "show" class to DIV
+  success.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ success.className = success.className.replace("show", ""); }, 2000);
   } else {
     $('.alert').removeClass('d-none').addClass('show');
   }
